b9b16e8a7bf56e0206cd8da4392065fafa982a46
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-06-22 20:31:11 +0700

    - Update

--- source demo/03-form-validation/my-app/src/app/app.component.ts
+++ source demo/03-form-validation/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  comboBox: string;
+  firstName: string;
+  lastName: string;
+  isRequired: boolean;
+
+  constructor(){
+    this.comboBox = '';
+  }
+  @ViewChild('form') form: NgForm;
+  save(){
+    this.form.form.markAsPristine();
+  }
 }
