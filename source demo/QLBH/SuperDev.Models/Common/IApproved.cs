97e0ee40a223901518a5083fafb9a77cf9f8c232
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-08-06 20:09:18 +0700

    - Update source demo

---
+++ source demo/QLBH/SuperDev.Models/Common/IApproved.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SuperDev.Models
+{
+    public interface IApprover
+    {
+        DateTime ApprovedDate { get; set; }
+
+        Guid? ApprovedBy { get; set; }
+
+        User Approver { get; set; }
+    }
+}
